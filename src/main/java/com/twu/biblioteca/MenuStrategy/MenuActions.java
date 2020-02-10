dca4b28bceb058447cb66275def9107b64dca636
Hareini Sreethi <hareini.manisekaran@thoughtworks.com>
2020-02-11 01:04:07 +0530

    Add | Menu Actions interface to perform actions according to menu option chosen

---
+++ src/main/java/com/twu/biblioteca/MenuStrategy/MenuActions.java
@@ -0,0 +1,9 @@
+package com.twu.biblioteca.MenuStrategy;
+
+import com.twu.biblioteca.Library;
+
+import java.util.Scanner;
+
+public interface MenuActions {
+    void performAction(Library library, Scanner scanner);
+}
