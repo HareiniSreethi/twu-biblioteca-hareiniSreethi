357be4d299d66b1b9c2134953ae2a62c8179ed9c
Hareini Sreethi <hareini.manisekaran@thoughtworks.com>
2020-02-11 18:19:11 +0530

    Refactor | Changed system out with custom printer function

--- src/main/java/com/twu/biblioteca/Menu.java
+++ src/main/java/com/twu/biblioteca/Menu.java
@@ -1,10 +1,12 @@
 package com.twu.biblioteca;
 
+import static com.twu.biblioteca.CustomInputOutput.printOutput;
+
 public class Menu {
     // TODO - why have "menu" in the method name? - DONE
     public void displayOptions() { // TODO - that sort of depends on implementation. foreach loop can be parallelized too. for-iter can't.
         for (MenuOptions menu : MenuOptions.values()) { // TODO - we use foreach loops when we don't care about indexes. When you write for loop - you're telling me indexes are important. in foreach, you're saying they not. - DONE
-            System.out.println(menu.getOptionNumber() + ". " + menu.getOptionValue()); // TODO - very confusing line. why? mixing integer addition with string addition -DONE
+            printOutput(menu.getOptionNumber() + ". " + menu.getOptionValue()); // TODO - very confusing line. why? mixing integer addition with string addition -DONE
         }
     }
 }
