1dda631a5c5508e2f94201e63be56b78e52fde44
Hareini Sreethi <hareini.manisekaran@thoughtworks.com>
2020-02-11 16:35:09 +0530

    Refactor | Changed for loops to streams to check out book from library

--- src/main/java/com/twu/biblioteca/Library.java
+++ src/main/java/com/twu/biblioteca/Library.java
@@ -4,11 +4,11 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Library {
-    List<Book> availableBooks = new ArrayList<>();
+    List<Book> availableBooks;
     List<Book> checkedoutBooks = new ArrayList<>();
 
     public Library(List<Book> availableBooks) {
-         // TODO - hardcoded inside. - DONE
+        // TODO - hardcoded inside. - DONE
         this.availableBooks = availableBooks;
     }
 
@@ -20,29 +20,25 @@ public class Library {
     }
 
     public void checkoutBook(String bookName) {
-        for (Book book : availableBooks) { // TODO - get rid of the loop? somehow. equals implementation - without loop. else streams. go read about it.
-            if (book.checkBookByName(bookName)) {
-                checkedoutBooks.add(book);
-                System.out.println("Thank you! Enjoy the book");
-                return;
-            }
-        }
-        System.out.println("Sorry, that book is not available");
+        // TODO - get rid of the loop? somehow. equals implementation - without loop. else streams. go read about it.- DONE
+        availableBooks.stream()
+                .filter(book -> book.checkBookByName(bookName))
+                .findFirst()
+                .ifPresentOrElse(book -> {
+                    checkedoutBooks.add(book);
+                    System.out.println("Thank you! Enjoy the book");
+                }, () -> System.out.println("Sorry, that book is not available"));
     }
 
 
     public void returnBook(String bookName) {
-        for(Book book : checkedoutBooks){
-            if(book.checkBookByName(bookName)) {
+        for (Book book : checkedoutBooks) {
+            if (book.checkBookByName(bookName)) {
                 checkedoutBooks.remove(book);
                 System.out.println("Thank you for returning the book");
                 return;
             }
         }
-            System.out.println("That is not a valid book to return");
-    }
-
-    public void checkoutBookByAuthor(String author) {
-        Book book = new Book(null, author, null); // TODO - 1. equals is broken (implementation problem as of now). 2.-DONE
+        System.out.println("That is not a valid book to return");
     }
 }
