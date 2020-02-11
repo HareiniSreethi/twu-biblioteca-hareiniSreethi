68850d324abb98e8b4de751cc4ac03875662fb71
Hareini Sreethi <hareini.manisekaran@thoughtworks.com>
2020-02-11 23:04:30 +0530

    Add | Requirement2.1 | Movie class with function to view movie details

---
+++ src/main/java/com/twu/biblioteca/Movie.java
@@ -0,0 +1,22 @@
+package com.twu.biblioteca;
+
+import static com.twu.biblioteca.CustomInputOutput.printOutput;
+
+public class Movie {
+    private final String name;
+    private final String year;
+    private final String director;
+    private final String rating;
+
+    public Movie(String name, String year, String director, String rating) {
+        this.name = name;
+        this.year = year;
+        this.director = director;
+        this.rating = rating;
+    }
+
+    public void viewMovieDetails() {
+        String columnSeparator = " | ";
+        printOutput(this.name + columnSeparator + this.year + columnSeparator + this.director + columnSeparator + this.rating);
+    }
+}
---
+++ src/test/java/com/twu/biblioteca/MovieTest.java
@@ -0,0 +1,38 @@
+package com.twu.biblioteca;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MovieTest {
+
+    private ByteArrayOutputStream outContent;
+    private PrintStream originalOut;
+
+    @BeforeEach
+    public void setUp() {
+        outContent = new ByteArrayOutputStream();
+        originalOut = System.out;
+        System.setOut(new PrintStream(outContent));
+    }
+
+    @AfterEach
+    public void restoreStreams() {
+        System.setOut(originalOut);
+    }
+
+    @Test
+    void shouldBeAbleToViewMovieDetails() {
+        Movie movie = new Movie("Titanic", "1997", "James Cameron", "8");
+        String expectedOutput = "Titanic | 1997 | James Cameron | 8\n";
+
+        movie.viewMovieDetails();
+
+        assertEquals(expectedOutput, outContent.toString());
+    }
+}
