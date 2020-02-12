b15a97b9ceed9174cf5aef132e815ffc9f7856c2
Hareini Sreethi <hareini.manisekaran@thoughtworks.com>
2020-02-12 10:44:04 +0530

    Add | Requirement 2.2 | New strategy to perform actions of checkout movie

---
+++ src/main/java/com/twu/biblioteca/MenuStrategy/CheckoutMovie.java
@@ -0,0 +1,16 @@
+package com.twu.biblioteca.MenuStrategy;
+
+import com.twu.biblioteca.Library;
+
+import java.util.Scanner;
+
+import static com.twu.biblioteca.CustomInputOutput.printOutput;
+
+public class CheckoutMovie implements MenuActions {
+    @Override
+    public void performAction(Library library, Scanner scanner) {
+        printOutput("Enter book name to check out : ");
+        String movieName = scanner.nextLine();
+        library.checkoutMovie(movieName);
+    }
+}
